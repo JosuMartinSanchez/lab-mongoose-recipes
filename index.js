6c8cc5d0287b15ffc65e25e7c77fbcb9e820c843
Juan Jose Ferran Carmona <jferran@gmail.com>
2022-05-05 17:55:32 +0200

    trying with await

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
+
+
 mongoose.connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
@@ -69,4 +71,59 @@ mongoose.connect(MONGODB_URI)
     mongoose.connection.close()
   });
 
+
+
+
+  //con async await
+
+//   async function modificateDB() {
+//     // ... Your code here
+    
+//     try{
+//       mongoose.connect(MONGODB_URI)
+//       console.log(`Connected to the database: ""`);
+      
+//       await Recipe.deleteMany()
+// /*
+//       await Recipe.create(
+//         {
+//         title: "Asian Glazed Chicken Thighs",
+//         level: "Amateur Chef",
+//         ingredients: [
+//           "1/2 cup rice vinegar",
+//           "5 tablespoons honey",
+//           "1/3 cup soy sauce (such as Silver Swan®)",
+//           "1/4 cup Asian (toasted) sesame oil",
+//           "3 tablespoons Asian chili garlic sauce",
+//           "3 tablespoons minced garlic",
+//           "salt to taste",
+//           "8 skinless, boneless chicken thighs"
+//         ],
+//         cuisine: "Asian",
+//         dishType: "main_course",
+//         image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+//         duration: 40,
+//         creator: "Chef LePapu"
+//       })
+// */
+//       let response = await Recipe.insertMany(data)
+
+//       response.forEach((element)=>{
+//         console.log(element.title)
+//       })
+
+//       await Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'},{duration:100},{new:true})
+
+//       console.log('La duración ha sido cambiada');
+//       await Recipe.deleteOne({title:'Carrot Cake'})
+
+//       console.log('Carrot Cake ha sido eliminada');
+//       mongoose.connection.close()      
+//     }
+//     catch (error){
+//       console.log(error)
+//       mongoose.connection.close()
+//     }
+//   }
+//   modificateDB();
 
